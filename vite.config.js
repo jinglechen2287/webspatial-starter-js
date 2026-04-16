19bb1f6951b8a851ae30da7d9560c155cddabd09
jingle chen <47862618+jinglechen2287@users.noreply.github.com>
2026-04-16 00:31:51 -0400

    include setup for the build and deploy step

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,24 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
+import basicSsl from "@vitejs/plugin-basic-ssl";
 import webspatial from "@webspatial/vite-plugin";
 import { createHtmlPlugin } from "vite-plugin-html";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    webspatial(),
-    createHtmlPlugin({
-      inject: {
-        data: {
-          XR_ENV: process.env.XR_ENV,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, ".", "");
+  return {
+    plugins: [
+      react(),
+      basicSsl(),
+      webspatial(),
+      createHtmlPlugin({
+        inject: {
+          data: {
+            XR_ENV: env.XR_ENV,
+          },
         },
-      },
-    }),
-  ],
+      }),
+    ],
+  };
 });
